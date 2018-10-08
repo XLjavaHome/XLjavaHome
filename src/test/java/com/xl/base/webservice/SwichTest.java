83e1e05b84e965446962c29aa29a20d064b71a3e
xuli <78645574@qq.com>
2018-10-08 15:51:21 +0800

    dateTest

---
+++ src/test/java/com/xl/base/webservice/SwichTest.java
@@ -0,0 +1,17 @@
+package com.xl.base.webservice;
+
+import org.junit.Test;
+
+/**
+ * Created with 徐立.
+ *
+ * @author: 徐立
+ * @Date: 2018-10-08
+ * @Time: 14:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class SwichTest {
+    @Test
+    public void nullTest() {
+    }
+}
--- src/test/java/com/xl/calendar/DateTest.java
+++ src/test/java/com/xl/calendar/DateTest.java
@@ -1,9 +1,11 @@
 package com.xl.calendar;
 
-import lombok.extern.log4j.Log4j;
-import org.junit.Test;
-
+import com.xl.util.DateUtil;
+import java.util.Calendar;
 import java.util.Date;
+import lombok.extern.log4j.Log4j;
+import org.apache.commons.lang.time.DateUtils;
+import org.junit.Test;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,11 +18,37 @@ import java.util.Date;
 @Log4j
 public class DateTest {
     @Test
-    public void test() {
+    public void addDayTest() {
         Date d = new Date();
         Date d2 = d;
-        log.info(d.getMonth());
-        log.info(d.getDay());
-        log.info(d2.compareTo(d));//相等就输出
+        //当前日期加一天
+        Date date = DateUtils.addDays(d, 1);
+        System.out.println(DateUtil.formatDate(date));
+    }
+
+    /**
+     * 新日期=原日期，改变新日期后原日期也发送改变
+     */
+    @Test
+    public void data1Test() {
+        Calendar now = Calendar.getInstance();
+        Calendar newCal = now;
+        newCal.set(Calendar.DATE, 0);
+        System.out.println(DateUtil.formatDate(now.getTime(), "yyyy-MM-dd"));
+    }
+
+    /**
+     * 相等的日期就返回true
+     */
+    @Test
+    public void equalTest() {
+        Calendar now = Calendar.getInstance();
+        now.set(Calendar.HOUR, 2);
+        System.out.println(now.get(Calendar.ERA));
+        System.out.println(now.get(0));
+        System.out.println(now.get(1));
+        System.out.println(now.get(2));
+        System.out.println(DateUtils.isSameDay(now, Calendar.getInstance()));
+        System.out.println(org.apache.commons.lang3.time.DateUtils.isSameDay(now, Calendar.getInstance()));
     }
 }
