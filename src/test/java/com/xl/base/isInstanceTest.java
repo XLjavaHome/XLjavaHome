190962284c19c0201ed65c238062b92b3bbe67ed
xuli <78645574@qq.com>
2019-02-27 14:12:24 +0800

    init

---
+++ src/test/java/com/xl/base/isInstanceTest.java
@@ -0,0 +1,23 @@
+package com.xl.base;
+
+import org.junit.Test;
+
+/**
+ * Created with 徐立.
+ *
+ * @author 徐立
+ * @date 2019-02-27
+ * @time 14:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class isInstanceTest {
+    /**
+     * 看看是否能为空
+     */
+    @Test
+    public void isInstanceTest() {
+        System.out.println(Number.class.isInstance(1));
+        //null就是false
+        System.out.println(Number.class.isInstance(null));
+    }
+}
