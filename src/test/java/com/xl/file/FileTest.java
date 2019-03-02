423bcc9c4715b873e7889f864c33b6053e1ea8a5
徐立 <78645574@qq.com>
2019-03-02 15:43:45 +0800

    init

--- src/test/java/com/xl/file/FileTest.java
+++ src/test/java/com/xl/file/FileTest.java
@@ -1,6 +1,8 @@
 package com.xl.file;
 
 import com.xl.enumsupport.CharsetEnum;
+import com.xl.service.FileService;
+import com.xl.service.FileServiceImpl;
 import com.xl.util.FileUtil;
 import com.xl.util.StringUtil;
 import java.io.File;
@@ -25,12 +27,12 @@ public class FileTest {
         System.out.println(File.pathSeparator);       //;
         System.out.println(File.pathSeparatorChar);   //;
     }
-
+    
     @Test
     public void getDeskTest() {
         System.out.println(FileUtil.getDesktopPath());
     }
-
+    
     @Test
     public void encodeTest() throws IOException {
         List<File> files = FileUtil.queryAll("D:\\code\\bbs1\\src\\main\\java\\cms\\bean\\data");
@@ -42,5 +44,29 @@ public class FileTest {
             if (fileEncode != null && StringUtil.equals(CharsetEnum.UTF8.getValue(), fileEncode.name())) {
             }
         }
+    }
+    
+    /**
+     * 拷贝
+     */
+    @Test
+    public void copyTest() {
+        //遍历目录
+        String filePath = "E:\\download\\Document-master";
+        List<File> files = FileUtil.queryAll(filePath);
+        //FileUtil.print(files);
+        //    将文件复制
+        FileService service = new FileServiceImpl();
+        int num = 0;
+        for (File file : files) {
+            File target = new File(filePath, file.getName());
+            if (service.copy(file, target)) {
+                num++;
+                System.out.println(String.format("成功复制:%s", file.getAbsolutePath()));
+            } else {
+                System.out.println(String.format("复制失败:%s", file.getAbsolutePath()));
+            }
+        }
+        System.out.println(String.format("成功复制了%s个文件", num));
     }
 }
