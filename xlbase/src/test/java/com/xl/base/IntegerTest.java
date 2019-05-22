8cd8b813ddcfe192782c59912c0d65a9460b893f
xuli <78645574@qq.com>
2019-05-22 15:16:12 +0800

    init

---
+++ xlbase/src/test/java/com/xl/base/IntegerTest.java
@@ -0,0 +1,20 @@
+package com.xl.base;
+
+import org.junit.Test;
+
+/**
+ * Created with 徐立.
+ *
+ * @author 徐立
+ * @date 2019-05-22
+ * @time 15:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class IntegerTest {
+    @Test
+    public void 空测试() {
+        Integer a = 4;
+        //可以这样写
+        System.out.println(a == null);
+    }
+}
