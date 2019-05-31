ceec360e2ec3e8c2e05f6a28c982c78d607ecad0
xuli <78645574@qq.com>
2019-05-31 15:41:32 +0800

    init

--- xlbase/src/test/java/com/xl/base/TryTest.java
+++ xlbase/src/test/java/com/xl/base/TryTest.java
@@ -3,6 +3,7 @@ package com.xl.base;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import lombok.extern.log4j.Log4j;
 import org.junit.Test;
 
 /**
@@ -13,6 +14,7 @@ import org.junit.Test;
  * @time 23:19
  * To change this template use File | Settings | File Templates.
  */
+@Log4j
 public class TryTest {
     @Test
     public void name() {
@@ -23,5 +25,23 @@ public class TryTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    @Test
+    public void tryAndTry() {
+        try {
+            try {
+                test();
+            } catch (Exception e) {
+                //只会打印这个异常
+                log.info("这是异常2");
+            }
+        } catch (Exception e) {
+            log.info("你好");
+        }
+    }
+    
+    private void test() {
+        int i = 1 / 0;
     }
 }
