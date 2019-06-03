5bdac4e93e1e02604465288633f662df9edc73e2
xuli <78645574@qq.com>
2019-06-03 09:36:50 +0800

    init

---
+++ xlbase/src/test/java/com/xl/base/BigDecimalTest.java
@@ -0,0 +1,21 @@
+package com.xl.base;
+
+import java.math.BigDecimal;
+import org.junit.Test;
+
+/**
+ * Created with 徐立.
+ *
+ * @author 徐立
+ * @date 2019-06-03
+ * @time 9:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class BigDecimalTest {
+    @Test
+    public void name() {
+        BigDecimal a = new BigDecimal("2");
+        BigDecimal b = new BigDecimal("3");
+        System.out.println(a.add(b));
+    }
+}
---
+++ xlbase/src/test/java/com/xl/base/NumberTest.java
@@ -0,0 +1,24 @@
+package com.xl.base;
+
+import org.junit.Test;
+
+/**
+ * Created with 徐立.
+ *
+ * @author 徐立
+ * @date 2019-06-03
+ * @time 9:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class NumberTest {
+    /**
+     * 说两个值相 异结果为真。相同为假
+     */
+    @Test
+    public void 异或() {
+        //a=15的二进制是1111，b=2的二进制是0010，经过异或运算后，得到的二进制数为1101，转换为十进制就是13。
+        int a = 15;
+        int b = 2;
+        System.out.println("a^b=" + (a ^ b));
+    }
+}
