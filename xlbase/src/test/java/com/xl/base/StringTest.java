52dd034e8d77e0891cd487ad04e6c2f221001ff3
xuli <78645574@qq.com>
2019-07-10 09:35:03 +0800

    init

--- xlbase/src/test/java/com/xl/base/StringTest.java
+++ xlbase/src/test/java/com/xl/base/StringTest.java
@@ -1,6 +1,8 @@
 package com.xl.base;
 
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import org.apache.commons.lang.StringUtils;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,6 +14,23 @@ import org.junit.jupiter.api.Test;
  * To change this template use File | Settings | File Templates.
  */
 public class StringTest {
+    @Test
+    void splitTest() {
+        //    空格是否能去掉,不能去掉
+        //暂时没有发现能去掉空格的
+        String str = "  空格 ,111 ";
+        String[] split = StringUtils.split(str, ",");
+        printArray(split);
+        String[] strings = StringUtils.splitByWholeSeparator(str, ",", 0);
+        printArray(strings);
+        strings = StringUtils.splitPreserveAllTokens(str, " ,", 0);
+        printArray(strings);
+    }
+    
+    private void printArray(String[] split) {
+        System.out.println(Arrays.toString(split));
+    }
+    
     @Test
     void format() {
         //静态方法是没有对象的。
