5f53f1096c83d110ed8ddd450fae26b3e50060b0
xuli <78645574@qq.com>
2019-07-18 18:45:34 +0800

    Stream

--- xlbase/src/test/java/com/xl/StreamTest.java
+++ xlbase/src/test/java/com/xl/StreamTest.java
@@ -1,8 +1,11 @@
 package com.xl;
 
 import com.xl.entity.Student;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,27 +17,53 @@ import org.junit.jupiter.api.Test;
  * To change this template use File | Settings | File Templates.
  */
 public class StreamTest {
-    @Test
-    void array() {
-        Set<Student> list = new HashSet<>();
+    private List<Student> students = new ArrayList<>();
+    
+    public StreamTest() {
         for (int i = 0; i < 10; i++) {
             Student s = new Student();
             s.setId(i);
-            s.setSex("");
+            s.setSex("x");
             s.setName("姓名" + i);
             s.setAddress("");
             s.setAge(i);
             s.setPhone("");
-            list.add(s);
+            students.add(s);
         }
-        String[] arr2 = list.stream().map(Student::getName).toArray(String[]::new);
-        printArray(arr2);
-        String[] objects = list.stream().map(Student::getName).toArray(size -> new String[size]);
+    }
+    
+    @Test
+    void 转换大写() {
+        String[] collect = students.stream().map(x -> x.getSex().toUpperCase()).toArray(String[]::new);
+        printArray(collect);
+    }
+    
+    private void printArray(Object[] objects) {
+        System.out.println(Arrays.toString(objects));
+    }
+    
+    @Test
+    void array() {
+        // 2. Arrays
+        String[] strArray = new String[]{"a", "b", "c"};
+        Stream<String> stream = Stream.of(strArray);
+        printStream(stream);
+        stream = Arrays.stream(strArray);
+        printStream(stream);
+    }
+    
+    private void printStream(Stream<String> stream) {
+        Object[] objects = stream.toArray();
         printArray(objects);
     }
     
-    private void printArray(String[] objects) {
-        System.out.println(Arrays.toString(objects));
+    @Test
+    void toArray() {
+        //别的类型会报错，类型推到错误
+        String[] arr2 = students.stream().map(Student::getName).toArray(String[]::new);
+        printArray(arr2);
+        String[] objects = students.stream().map(Student::getName).toArray(size -> new String[size]);
+        printArray(objects);
     }
     
     @Test
@@ -52,5 +81,9 @@ public class StreamTest {
         //2.stream流
         List<Integer> newList2 = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
         System.out.println(newList2);
+    }
+    
+    @Test
+    void sort() {
     }
 }
