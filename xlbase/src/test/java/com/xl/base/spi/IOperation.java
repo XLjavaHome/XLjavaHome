4611d88bfc16248d469acdcba9f026af11a21dfb
徐立 <78645574@qq.com>
2019-09-12 00:02:20 +0800

    spi的示例

---
+++ xlbase/src/test/java/com/xl/base/spi/IOperation.java
@@ -0,0 +1,13 @@
+package com.xl.base.spi;
+
+/**
+ * Created with 徐立.
+ *
+ * @author 徐立
+ * @date 2019-09-11
+ * @time 23:43
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IOperation {
+    int operation(int numberA, int numberB);
+}
---
+++ xlbase/src/test/java/com/xl/base/spi/PlusOperationImpl.java
@@ -0,0 +1,16 @@
+package com.xl.base.spi;
+
+/**
+ * Created with 徐立.
+ *
+ * @author 徐立
+ * @date 2019-09-11
+ * @time 23:43
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlusOperationImpl implements IOperation {
+    @Override
+    public int operation(int numberA, int numberB) {
+        return numberA + numberB;
+    }
+}
