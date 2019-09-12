9270c34341015b61c1f33cb94f3cdd0881aa4447
xuli <78645574@qq.com>
2019-09-12 11:54:01 +0800

    移除spring

--- xlbase/src/main/java/com/xl/service/SpringService.java
+++ xlbase/src/main/java/com/xl/service/SpringService.java
@@ -1,6 +1,5 @@
 package com.xl.service;
 
-import com.xl.base.SpringDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
