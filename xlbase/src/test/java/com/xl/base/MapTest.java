09c744a45d05f2992ae63c30d551ba198cc89d65
徐立 <78645574@qq.com>
2019-10-11 23:41:48 +0800

    merge

--- xlbase/src/test/java/com/xl/base/MapTest.java
+++ xlbase/src/test/java/com/xl/base/MapTest.java
@@ -1,5 +1,7 @@
 package com.xl.base;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.collections.map.CaseInsensitiveMap;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +19,21 @@ public class MapTest {
         CaseInsensitiveMap map = new CaseInsensitiveMap();
         map.put("hello", 1);
         map.put("Hello", 2);
+        System.out.println(map);
+    }
+    
+    /**
+     * 1. 可以保存map中没有的key
+     * 2. 如果已有则执行第3个参数 lamda表达式
+     * 3. 第三个参数 增强函数BiFunction<T, U, R>  旧value，新value
+     */
+    @Test
+    void merge() {
+        Map<Integer, String> map = new HashMap<>(10);
+        map.put(1, "张三");
+        map.merge(2, "李四", (s, s2) -> s + "、" + s2);
+        map.merge(2, "张三", (s, s2) -> s + "、" + s2);
+        map.merge(3, "张三", (s, s2) -> s + "、" + s2);
         System.out.println(map);
     }
 }
--- xljxl/src/test/java/JxlTest.java
+++ xljxl/src/test/java/JxlTest.java
@@ -6,8 +6,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import jxl.Cell;
+import jxl.CellView;
 import jxl.format.CellFormat;
 import jxl.format.UnderlineStyle;
+import jxl.write.Number;
 import jxl.write.*;
 import lombok.Data;
 import org.junit.Test;
@@ -26,7 +28,7 @@ public class JxlTest {
         File file = FileUtil.getTempFile(".xls");
         OutputStream os = new BufferedOutputStream(new FileOutputStream(file));
         WritableWorkbook workbook = jxl.Workbook.createWorkbook(os);
-        //sheet1.mergeCells(0, 1, 1, 1);//添加合并单元格:第一个参数是起始列;第二个参数是起始行;第三个参数是终止列;第四个参数是终止行
+        //sheet.mergeCells(0, 1, 1, 1);//添加合并单元格:第一个参数是起始列;第二个参数是起始行;第三个参数是终止列;第四个参数是终止行
         // 通过函数WritableFont（）设置字体样式
         // 第一个参数表示所选字体
         // 第二个参数表示字体大小
@@ -34,50 +36,50 @@ public class JxlTest {
         // 第四个参数表示是否斜体
         // 第五个参数表示下划线样式
         // 第六个参数表示颜色样式
-        WritableSheet sheet1 = workbook.createSheet("客户详情", 0);//第一个sheet名
+        WritableSheet sheet = workbook.createSheet("客户详情", 0);//第一个sheet名
         WritableFont wf = new WritableFont(WritableFont.TIMES, 16, WritableFont.BOLD, false, UnderlineStyle.NO_UNDERLINE,
                                            Colour.BLACK);
         CellFormat cf = new WritableCellFormat(wf);
-        sheet1.addCell(new Label(0, 0, "客户详情", cf));
-        sheet1.addCell(new Label(0, 2, "客户编号"));
-        sheet1.addCell(new Label(1, 2, "客户名称"));
+        sheet.addCell(new Label(0, 0, "客户详情", cf));
+        sheet.addCell(new Label(0, 2, "客户编号"));
+        sheet.addCell(new Label(1, 2, "客户名称"));
         List<Student> list = initStudent();
         //根据内容自动设置列宽(内容为英文时)
-        //CellView cellView = new CellView();
-        //cellView.setAutosize(true);
-        //sheet1.setColumnView(0, cellView);
-        //sheet1.setColumnView(1, cellView);
+        CellView cellView = new CellView();
+        cellView.setAutosize(true);
+        sheet.setColumnView(0, cellView);
+        sheet.setColumnView(1, cellView);
         // 生成主体内容
         Map<String, MerEntity> map = new HashMap<>();
         for (int i = 0; i < list.size(); i++) {
             Student stu = list.get(i);
-            //sheet1.setColumnView(0, bstrLength1.length+2);
-            //sheet1.setColumnView(1, bstrLength2.length+2); //根据内容自动设置列宽(内容可以为中文)
+            //sheet.setColumnView(0, bstrLength1.length+2);
+            //sheet.setColumnView(1, bstrLength2.length+2); //根据内容自动设置列宽(内容可以为中文)
             int row = i + 3;
-            sheet1.addCell(new Label(0, row, stu.getId() + ""));
-            sheet1.addCell(new Label(1, row, stu.getName()));
+            int cell = 0;
+            sheet.addCell(new Label(cell, row, stu.getId() + ""));
+            sheet.addCell(new Label(++cell, row, stu.getName()));
+            Number number = new Number(++cell, row, stu.getAge());
+            sheet.addCell(number);
             String address = stu.getAddress();
             MerEntity merEntity = map.get(address);
+            //
             if (merEntity == null) {
                 merEntity = new MerEntity();
-                merEntity.setCol(2);
+                merEntity.setCol(4);
                 merEntity.setStart(row);
                 merEntity.setEnd(row);
                 merEntity.setContent(address);
                 map.put(address, merEntity);
-                sheet1.addCell(new Label(2, row, address));
+                sheet.addCell(new Label(++cell, row, address));
             } else {
                 merEntity.setEnd(row);
+                sheet.mergeCells(merEntity.getCol(), merEntity.getStart(), merEntity.getCol(), merEntity.getEnd());
             }
         }
         //合并单元格 todo 怎么居中？
         // TODO 2019/10/9 23:42 徐立 如果单元格有一样的内容则不能合并
-        for (MerEntity merEntity : map.values()) {
-            sheet1.mergeCells(merEntity.getCol(), merEntity.getStart(), merEntity.getCol(), merEntity.getEnd());
-            Cell cell = sheet1.getCell(merEntity.getCol(), merEntity.getStart());
-            CellFormat cellFormat = cell.getCellFormat();
-            jxl.format.VerticalAlignment verticalAlignment = cellFormat.getVerticalAlignment();
-        }
+        //aa(sheet, map);
         workbook.write(); // 写入文件
         workbook.close();
         os.close(); // 关闭流
@@ -90,6 +92,7 @@ public class JxlTest {
             Student student = new Student();
             student.setId(i);
             student.setName("测试" + i);
+            student.setAge(i % 60);
             resulit.add(student);
         }
         for (int i = 0, resulitSize = 10; i < resulitSize; i++) {
@@ -105,6 +108,15 @@ public class JxlTest {
             student.setAddress("测试");
         }
         return resulit;
+    }
+    
+    private void aa(WritableSheet sheet1, Map<String, MerEntity> map) throws WriteException {
+        for (MerEntity merEntity : map.values()) {
+            sheet1.mergeCells(merEntity.getCol(), merEntity.getStart(), merEntity.getCol(), merEntity.getEnd());
+            Cell cell = sheet1.getCell(merEntity.getCol(), merEntity.getStart());
+            CellFormat cellFormat = cell.getCellFormat();
+            jxl.format.VerticalAlignment verticalAlignment = cellFormat.getVerticalAlignment();
+        }
     }
     
     @Data
